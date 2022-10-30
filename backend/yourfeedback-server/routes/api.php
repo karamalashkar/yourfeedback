125c60ff535d73588d3f15b951d0eff8edb13ed9
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-30 17:09:51 +0200

    Backend <> Adding add new business route

--- backend/yourfeedback-server/routes/api.php
+++ backend/yourfeedback-server/routes/api.php
@@ -3,5 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\BusinessController;
 
 Route::post('/add_user',[UserController::class,'addUser']);
+Route::post('/add_business',[BusinessController::class,'addBusiness']);
