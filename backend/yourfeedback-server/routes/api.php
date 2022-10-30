674438ded90b0aa34398ffbb3dcb4fbc7e687152
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-30 21:38:25 +0200

    Backend <> Adding search for a business route

--- backend/yourfeedback-server/routes/api.php
+++ backend/yourfeedback-server/routes/api.php
@@ -13,7 +13,8 @@ Route::group(["prefix"=> "business"], function(){
     Route::get('/get_business/{id}',[BusinessController::class,'getBusiness']);
     Route::post('/edit_business',[BusinessController::class,'editBusiness']);
     Route::get('/business_category/{id}',[BusinessController::class,'businessByCategory']);
-    Route::post('/near_business',[BusinessController::class,'nearBusiness']);        
+    Route::post('/near_business',[BusinessController::class,'nearBusiness']);
+    Route::get('/search_business/{word}',[BusinessController::class,'searchBusiness']);        
 });
 
 Route::post('/add_user',[UserController::class,'addUser']);
