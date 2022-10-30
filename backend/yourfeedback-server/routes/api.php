285fda5bf916fb0b4a3b100319b1198ad7e367b8
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-30 18:24:37 +0200

    Backend <> Adding get user route

--- backend/yourfeedback-server/routes/api.php
+++ backend/yourfeedback-server/routes/api.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\BusinessController;
 
+Route::group(["prefix"=> "user"], function(){
+    Route::get('/get_user/{id}',[UserController::class,'getUser']);
+});
+
 Route::group(["prefix"=> "business"], function(){
     Route::get('/get_business/{id}',[BusinessController::class,'getBusiness']);
     Route::post('/edit_business',[BusinessController::class,'editBusiness']);        
