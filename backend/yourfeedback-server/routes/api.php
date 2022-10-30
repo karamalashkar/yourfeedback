35ac9275e996f30c44f5ac828377ff19f1e24334
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-30 18:03:07 +0200

    Backend <> Adding edit business route

--- backend/yourfeedback-server/routes/api.php
+++ backend/yourfeedback-server/routes/api.php
@@ -5,5 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\BusinessController;
 
+Route::group(["prefix"=> "business"], function(){
+    Route::post('/edit_business',[BusinessController::class,'editBusiness']);        
+});
+
 Route::post('/add_user',[UserController::class,'addUser']);
 Route::post('/add_business',[BusinessController::class,'addBusiness']);
