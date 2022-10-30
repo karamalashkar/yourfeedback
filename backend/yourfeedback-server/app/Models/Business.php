beb9152aa7c5f89a83c52055a66fb430195731b6
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-30 23:08:10 +0200

    Backend <> Adding relation with user  Model

--- backend/yourfeedback-server/app/Models/Business.php
+++ backend/yourfeedback-server/app/Models/Business.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use App\Models\User;
 use App\Models\Category;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -16,5 +17,9 @@ class Business extends Model
 
     public function category(){
         return $this->belongsTo(Category::class);
+    }
+
+    public function userDiscount(){
+        return $this->belongsToMany(User::class,'discounts')->withPivot(['value'])->withTimestamps();
     }
 }
