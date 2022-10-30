b34d2f454c4624703db278f353490c8e84e62078
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-30 19:22:49 +0200

    Backend <> Adding get near businesses route

--- backend/yourfeedback-server/routes/api.php
+++ backend/yourfeedback-server/routes/api.php
@@ -12,7 +12,8 @@ Route::group(["prefix"=> "user"], function(){
 Route::group(["prefix"=> "business"], function(){
     Route::get('/get_business/{id}',[BusinessController::class,'getBusiness']);
     Route::post('/edit_business',[BusinessController::class,'editBusiness']);
-    Route::get('/business_category/{id}',[BusinessController::class,'businessByCategory']);        
+    Route::get('/business_category/{id}',[BusinessController::class,'businessByCategory']);
+    Route::post('/near_business',[BusinessController::class,'nearBusiness']);        
 });
 
 Route::post('/add_user',[UserController::class,'addUser']);
