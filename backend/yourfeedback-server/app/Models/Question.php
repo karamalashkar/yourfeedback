e0ade7be8897d0c86937c535b5010cf61ff78c77
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-31 02:17:23 +0200

    Backend <> Adding relation with feedback Model

--- backend/yourfeedback-server/app/Models/Question.php
+++ backend/yourfeedback-server/app/Models/Question.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 use App\Models\Category;
+use App\Models\Feedback;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -12,5 +13,9 @@ class Question extends Model
 
     public function category(){
         return $this->belongsTo(Category::class);
+    }
+
+    public function feedback(){
+        return $this->belongsTo(Feedback::class);
     }
 }
