ecc97026f85f62a175437ee945aa265dc08a20b3
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-31 17:42:01 +0200

    Backend <> Editing get near business route

--- backend/yourfeedback-server/routes/api.php
+++ backend/yourfeedback-server/routes/api.php
@@ -20,7 +20,7 @@ Route::group(["prefix"=> "business"], function(){
     Route::get('/get_business/{id}',[BusinessController::class,'getBusiness']);
     Route::post('/edit_business',[BusinessController::class,'editBusiness']);
     Route::get('/business_category/{id}',[BusinessController::class,'businessByCategory']);
-    Route::post('/near_business',[BusinessController::class,'nearBusiness']);
+    Route::get('/near_business/{latitude}/{longitude}',[BusinessController::class,'nearBusiness']);
     Route::get('/search_business/{word}',[BusinessController::class,'searchBusiness']);
     Route::get('/count_discount/{id}',[BusinessController::class,'countDiscount']);
     Route::get('/count_feedback/{id}',[BusinessController::class,'countFeedback']);
