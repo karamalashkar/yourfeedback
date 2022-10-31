1b093752e9ecb3bee0b1525f89a6d9454f765de3
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-31 02:27:56 +0200

    Backend <> Adding get discount route

--- backend/yourfeedback-server/routes/api.php
+++ backend/yourfeedback-server/routes/api.php
@@ -25,6 +25,7 @@ Route::group(["prefix"=> "business"], function(){
     Route::get('/count_discount/{id}',[BusinessController::class,'countDiscount']);
     Route::get('/count_feedback/{id}',[BusinessController::class,'countFeedback']);
     Route::get('/get_feedback/{id}',[BusinessController::class,'getFeedback']);        
+    Route::get('/get_discount/{id}',[BusinessController::class,'getDiscount']);        
 });
 
 Route::post('/add_user',[UserController::class,'addUser']);
