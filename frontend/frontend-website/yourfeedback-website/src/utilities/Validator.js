7ab910f19a217abc59027450c1a0469bf15fb1f7
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-01 21:50:23 +0200

    Frontend <> Adding validate email function

---
+++ frontend/frontend-website/yourfeedback-website/src/utilities/Validator.js
@@ -0,0 +1,9 @@
+const emailValidation = (email) =>{
+    const regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if(!email || regex.test(email) === false){
+        return false
+    }
+    return true
+}
+
+export default emailValidation;
