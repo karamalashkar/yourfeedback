038169df0efb2c32b6447caa4a1fb99f62efb3ed
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-01 23:16:24 +0200

    Frontend <> Adding signup page route

--- frontend/frontend-website/yourfeedback-website/src/App.js
+++ frontend/frontend-website/yourfeedback-website/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter ,Routes, Route} from 'react-router-dom';
 import Landing from './pages/Landing';
+import Signup from './pages/Signup';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element= {<Landing />} />
+          <Route path='/signup' element= {<Signup />} />
         </Routes>
       </BrowserRouter>      
     </div>
