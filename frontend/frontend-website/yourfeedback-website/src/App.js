ad11cae5d0cf51b56d9c4e74d4db7eaa054516e3
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-02 16:28:35 +0200

    Frontend <> Adding discount page route

--- frontend/frontend-website/yourfeedback-website/src/App.js
+++ frontend/frontend-website/yourfeedback-website/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter ,Routes, Route} from 'react-router-dom';
 import Landing from './pages/Landing';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
+import Discount from './pages/Discount';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path='/' element= {<Landing />} />
           <Route path='/signup' element= {<Signup />} />
           <Route path='/home' element= {<Home />} />
+          <Route path='/discount' element= {<Discount />} />
         </Routes>
       </BrowserRouter>      
     </div>
