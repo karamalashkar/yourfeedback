4bf242cac6f51ae1da430364c6a8b72f66804e90
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-04 20:49:30 +0200

    Frontend <> Editing landing page

--- frontend/frontend-website/yourfeedback-website/src/pages/Landing.js
+++ frontend/frontend-website/yourfeedback-website/src/pages/Landing.js
@@ -20,15 +20,15 @@ const Landing = () =>{
         <>
             <Navbar />
             <div className='flex flex-col align-center xl:flex-row'>
-                <div className='xl:w-5/12 flex-col items-center'>
-                    <h1 className='mb-8 text-5xl font-bold'>Give me your <br/> feedback</h1>
+                <div className='xl:w-5/12 flex flex-col items-center'>
+                    <h1 className='mb-8 text-5xl font-bold text-center'>Give me your <br/> feedback</h1>
                     <h2 className="my-4 text-lg text-red-700 font-bold">{error}</h2>
-                    <form onSubmit={login}>
+                    <form onSubmit={login} className="w-full flex flex-col items-center">
                         <Input type='email' text='Email' setValue={setEmail} setError={setError} />
                         <Input type='password' text='Password' setValue={setPassword} setError={setError} />
                         <Button text='Sign In' />
                     </form>    
-                    <h4>Don't have an account? <a href=''>Join Now</a></h4>
+                    <h4>Don't have an account? <a href='/signup'>Join Now</a></h4>
                 </div>
                 <div className='my-4 flex justify-center xl:w-7/12'>
                     <img src='feedback.jpg' className='mt-4 rounded-xl' alt='feedback' />
