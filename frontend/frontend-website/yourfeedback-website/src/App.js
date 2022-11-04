d5538ef0587572fce85f1d443e0c4c784fee6e75
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-04 20:16:24 +0200

    Frontend <> Adding feedback page route

--- frontend/frontend-website/yourfeedback-website/src/App.js
+++ frontend/frontend-website/yourfeedback-website/src/App.js
@@ -5,6 +5,7 @@ import Signup from './pages/Signup';
 import Home from './pages/Home';
 import Discount from './pages/Discount';
 import Profile from './pages/Profile';
+import Feedback from './pages/Feedback';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path='/' element= {<Landing />} />
           <Route path='/signup' element= {<Signup />} />
           <Route path='/home' element= {<Home />} />
+          <Route path='/feedback' element= {<Feedback />} />
           <Route path='/discount' element= {<Discount />} />
           <Route path='/profile' element= {<Profile />} />
         </Routes>
