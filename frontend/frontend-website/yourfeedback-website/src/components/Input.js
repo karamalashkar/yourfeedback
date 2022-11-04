81a35681f87a05ba8f0699c9da1e377d48198f30
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-04 20:39:33 +0200

    Frontend <> Editing input component

--- frontend/frontend-website/yourfeedback-website/src/components/Input.js
+++ frontend/frontend-website/yourfeedback-website/src/components/Input.js
@@ -3,9 +3,9 @@ const Input = (props) =>{
         <>
             <input type={props.type} 
             placeholder={props.text}
-            onChange={e => props.setValue(e.target.value)} 
+            onChange={e => props.setValue(e.target.value)}
+            onFocus={() => props.setError('')}
             className='w-1/2 p-3 my-2 border-2 border-black rounded-xl' />   
-            <br/>
         </> 
     )
 }
