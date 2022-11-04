361aa5c15bc740b2d7cbb87e1201a66208e6eb1f
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-04 20:51:54 +0200

    Frontend <> Editing profile page

--- frontend/frontend-website/yourfeedback-website/src/pages/Profile.js
+++ frontend/frontend-website/yourfeedback-website/src/pages/Profile.js
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import Sidebar from "../components/Sidebar";
 import Topbar from "../components/Topbar";
-import Button from "../components/Button";
+import Editprofile from "../components/Editprofile";
 
 const Profile = ()=>{
+    const [isOpen,setOpen]=useState(false)
     return(
         <>
             <div className="flex h-auto lg:h-screen bg-gray-100">
@@ -11,6 +13,7 @@ const Profile = ()=>{
                 </div>
                 <div className="w-4/5">
                     <Topbar text={'Profile'} />
+                    <Editprofile open={isOpen} onClose={()=>setOpen(false)} />
                     <div className="flex flex-col items-center lg:flex-row my-12">
                         <div className="flex justify-center w-full xl:w-2/4">
                             <img src='market.jpg' alt='profile picture' className="w-3/4 h-96 rounded-xl shadow-2xl" />
@@ -19,7 +22,7 @@ const Profile = ()=>{
                             <h1 className="text-4xl font-bold">Market Name</h1>
                             <p className="mt-8 max-w-md h-auto text-lg">Bio</p>
                             <h2 className='my-8 text-lg'>Feedback Code: <span className="font-bold">Code</span></h2>
-                            <Button text={'Edit Profile'} />
+                            <button onClick={()=>setOpen(true)} className='w-1/2 p-3 mt-4 bg-red-700 text-white rounded-xl'>Edit Profile</button>    
                         </div>
                     </div>
                 </div>
