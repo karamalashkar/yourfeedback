da368ee7010a22a352bffb8f3c59b320dee86e78
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-07 18:07:29 +0200

    Frontend <> Adding validate password function

--- frontend/frontend-application/yourfeedback-application/utilities/Validator.js
+++ frontend/frontend-application/yourfeedback-application/utilities/Validator.js
@@ -1,3 +1,4 @@
+//email validation function
 const emailValidation = (email) =>{
     const regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
     if(!email || regex.test(email) === false){
@@ -6,4 +7,13 @@ const emailValidation = (email) =>{
     return true
 }
 
-export default emailValidation;
+//password validation function
+const passwordValidation = (password) =>{
+    var pattern=  /^[A-Za-z]\w{7,14}$/;
+    if(!password || !password.match(pattern)){
+        return false
+    }
+    return true
+}
+
+export {emailValidation, passwordValidation};
