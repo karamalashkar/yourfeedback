73f5829872b40d187734427a915a9f9cedee75ca
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-07 23:41:30 +0200

    Frontend <> Adding discount screen to bottom navigation

--- frontend/frontend-application/yourfeedback-application/navigations/BottomTabNavigation.js
+++ frontend/frontend-application/yourfeedback-application/navigations/BottomTabNavigation.js
@@ -3,9 +3,11 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import Entypo from 'react-native-vector-icons/Entypo';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Feather from 'react-native-vector-icons/Feather';
 import { colors } from "../constants/colors";
 import { HomeStackNavigation } from "./HomeStackNavigation";
 import FeedbackScreen from "../screens/feedback/FeedbackScreen";
+import DiscountScreen from "../screens/discount/DiscountScreen";
 
 export function BottomTabNavigation (){
     const BottomTabsNav = createBottomTabNavigator();
@@ -63,6 +65,28 @@ export function BottomTabNavigation (){
               tabBarIcon: ({ focused, color, size }) => (
                 <MaterialIcons
                   name={"feedback"}
+                  size={28}
+                  color={color}
+                />
+              ),
+              headerStyle:{
+                backgroundColor: colors.red,
+              },
+              headerTitleStyle:{
+                color: colors.white,
+                fontWeight: 'bold',
+                fontSize: 25
+              }
+            }}
+          />
+          <BottomTabsNav.Screen
+            name="DiscountScreen"
+            component={DiscountScreen}
+            options={{
+              title: 'Discount',
+              tabBarIcon: ({ focused, color, size }) => (
+                <Feather
+                  name={"credit-card"}
                   size={28}
                   color={color}
                 />
