86c2c3d98844c864782841f816d2db0e03b041b6
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-07 16:29:20 +0200

    Frontend <> Adding register screen to stack navigation

--- frontend/frontend-application/yourfeedback-application/navigations/StackNavigation.js
+++ frontend/frontend-application/yourfeedback-application/navigations/StackNavigation.js
@@ -2,6 +2,7 @@ import React, { useRef } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from "../screens/login/LoginScreen";
+import RegisterScreen from "../screens/register/RegisterScreen";
 
 export function StackNavigation() {
   const RootStackNav = createStackNavigator();
@@ -15,6 +16,11 @@ export function StackNavigation() {
         <RootStackNav.Screen
           name="Login"
           component={LoginScreen}
+        />
+
+        <RootStackNav.Screen
+          name="Register"
+          component={RegisterScreen}
         />
         
       </RootStackNav.Navigator>
