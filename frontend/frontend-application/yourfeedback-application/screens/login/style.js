3e80518e5f14c483504d2128a1e65dcac0a01b95
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-07 15:48:00 +0200

    Frontend <> Adding login screen style

---
+++ frontend/frontend-application/yourfeedback-application/screens/login/style.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const styles=StyleSheet.create({
+    component:{
+        width: '100%',
+        alignItems: 'center'
+    },
+    image:{
+        width: 250,
+        height: 150,
+        borderRadius: 10,
+    },
+    text:{
+        fontSize: 32,
+        fontWeight: 'bold',
+        margin: 20,
+        textAlign: 'center',
+    }
+})
