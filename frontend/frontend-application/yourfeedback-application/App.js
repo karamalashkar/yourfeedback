33e9f77ff4864b8d4a30ae840c474501438e2aa5
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-08 20:07:18 +0200

    Frontend <> Editing the application file

--- frontend/frontend-application/yourfeedback-application/App.js
+++ frontend/frontend-application/yourfeedback-application/App.js
@@ -1,20 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { BottomTabNavigation } from './navigations/BottomTabNavigation';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <BottomTabNavigation />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
