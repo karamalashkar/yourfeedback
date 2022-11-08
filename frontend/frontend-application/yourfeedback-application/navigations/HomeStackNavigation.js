bd314649c90d25d27a871c86e7412ccde0b20eba
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-08 17:31:25 +0200

    Frontend <> Adding form screen to home stack navigation

--- frontend/frontend-application/yourfeedback-application/navigations/HomeStackNavigation.js
+++ frontend/frontend-application/yourfeedback-application/navigations/HomeStackNavigation.js
@@ -2,6 +2,8 @@ import React, { useRef } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../screens/home/HomeScreen";
+import BusinessScreen from "../screens/business/BusinessScreen";
+import FormScreen from "../screens/form/FormScreen";
 
 export function HomeStackNavigation() {
   const RootStackNav = createStackNavigator();
@@ -9,12 +11,29 @@ export function HomeStackNavigation() {
 
   return (
     <NavigationContainer
-    ref={navigationRef} >
+    ref={navigationRef} 
+    independent={true} >
       <RootStackNav.Navigator >
 
         <RootStackNav.Screen
           name="Home"
           component={HomeScreen}
+          options={{
+            headerShown: false
+          }}  
+        />
+
+        <RootStackNav.Screen
+          name="Business"
+          component={BusinessScreen}
+        />
+
+        <RootStackNav.Screen
+          name="Form"
+          component={FormScreen}
+          options={{
+            headerLeft: () => {}
+          }}  
         />
         
       </RootStackNav.Navigator>
