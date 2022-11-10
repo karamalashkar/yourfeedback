21ed398e004794f0827e81301f3a7451c224a5fc
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-10 12:26:06 +0200

    Backend <> Editing businesses migration

--- backend/yourfeedback-server/database/migrations/2022_10_30_142834_businesses.php
+++ backend/yourfeedback-server/database/migrations/2022_10_30_142834_businesses.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('image')->default('');
             $table->integer('latitude');
             $table->integer('longitude');
+            $table->integer('feedback_code');
             $table->timestamps();
         });
     }
