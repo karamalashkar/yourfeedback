270f0f7be520efeedcc019d31d90f8e9208bb8a4
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-11 21:32:48 +0200

    Frontend <> Getting feedback forms

--- frontend/frontend-website/yourfeedback-website/src/pages/Feedback.js
+++ frontend/frontend-website/yourfeedback-website/src/pages/Feedback.js
@@ -1,8 +1,34 @@
 import Sidebar from "../components/Sidebar";
 import Topbar from "../components/Topbar";
 import Card from "../components/Card";
+import { getFeedback } from "../api/feedback";
+import { useEffect, useState } from "react";
 
 const Feedback = ()=>{
+    const business_id=localStorage.getItem('id');
+    const [data,setData]=useState('')
+
+    useEffect(()=>{
+        const feedback = async ()=>{
+            const result=await getFeedback(business_id);
+            setData(result)
+        }
+        feedback()
+    },[])
+
+    if(!data){
+        return(
+            <div className="flex h-full bg-gray-100">
+                <div className="w-1/5">
+                    <Sidebar />
+                </div>
+                <div className="w-4/5">
+                    <Topbar text={'Feedback'} />
+                    <img src='data.png' className="w-full"/>
+                </div>
+            </div>
+        )
+    }
     return(
         <>
             <div className="flex h-full bg-gray-100">
@@ -12,7 +38,14 @@ const Feedback = ()=>{
                 <div className="w-4/5">
                     <Topbar text={'Feedback'} />
                     <div className="flex flex-wrap justify-evenly">
-                        <Card />
+                        {Object.values(data).map((answer)=>{
+                            console.log(answer)
+                            return(
+                                <Card name={answer[0].user.name} 
+                                date={answer[0].created_at.substring(0,10)} 
+                                data={answer} />
+                            )
+                        })}
                     </div>
                 </div>
             </div>
@@ -21,3 +54,4 @@ const Feedback = ()=>{
 }
 
 export default Feedback;
+
