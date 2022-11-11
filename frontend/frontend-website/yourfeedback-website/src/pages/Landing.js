6ff73ea62ff28d7d1c4ee9b1413e61f9c23f1f48
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-11 19:34:08 +0200

    Frontend <> Checking login api response

--- frontend/frontend-website/yourfeedback-website/src/pages/Landing.js
+++ frontend/frontend-website/yourfeedback-website/src/pages/Landing.js
@@ -2,18 +2,32 @@ import { useState } from "react";
 import Navbar from "../components/Navbar";
 import Input from "../components/Input";
 import Button from "../components/Button";
+import { login } from "../api/login";
+import { useNavigate } from "react-router-dom";
 
 const Landing = () =>{
+    const navigate=useNavigate();
     const[email,setEmail]=useState('')
     const[password,setPassword]=useState('')
     const[error,setError]=useState('')
 
-    const login = (e)=>{
+    const loginBusiness = async (e)=>{
         e.preventDefault();
         if(!email || !password){
             setError('Please enter Email and Password')
             return null
-        }   
+        }
+
+        const post={email,password};
+        const result=await login(post);
+        if(result.response == 'success'){
+            localStorage.setItem('id',result.business)
+            localStorage.setItem('token',result.result.token)
+            navigate("/home")
+        }
+        else{
+            setError('Invalid Credential')
+        }
     }
     
     return(
@@ -23,7 +37,7 @@ const Landing = () =>{
                 <div className='xl:w-5/12 flex flex-col items-center'>
                     <h1 className='mb-8 text-5xl font-bold text-center'>Give me your <br/> feedback</h1>
                     <h2 className="my-4 text-lg text-red-700 font-bold">{error}</h2>
-                    <form onSubmit={login} className="w-full flex flex-col items-center">
+                    <form onSubmit={loginBusiness} className="w-full flex flex-col items-center">
                         <Input type='email' text='Email' setValue={setEmail} setError={setError} />
                         <Input type='password' text='Password' setValue={setPassword} setError={setError} />
                         <Button text='Sign In' />
