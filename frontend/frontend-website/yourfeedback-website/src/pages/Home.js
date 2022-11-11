15e13f86559ca8eac91c1cdaf9bfe013c85f2111
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-11 20:15:02 +0200

    Frontend <> Getting total number of feedback

--- frontend/frontend-website/yourfeedback-website/src/pages/Home.js
+++ frontend/frontend-website/yourfeedback-website/src/pages/Home.js
@@ -2,8 +2,22 @@ import Sidebar from "../components/Sidebar";
 import Topbar from "../components/Topbar";
 import Box from "../components/Box";
 import Chart from "../components/Chart";
+import { getCountFeedback } from "../api/countFeedback";
+import { useState,useEffect } from "react";
 
 const Home = ()=>{
+    const business_id=localStorage.getItem('id');
+    const [countF,setCountFeedback]=useState('')
+      
+    useEffect(()=>{
+        const countFeedback = async ()=>{
+            const result=await getCountFeedback(business_id);
+            setCountFeedback(result)
+        }
+
+        countFeedback();
+    },[])
+    
     return(
         <>
             <div className="flex h-auto lg:h-screen bg-gray-100">
@@ -13,7 +27,7 @@ const Home = ()=>{
                 <div className="w-4/5">
                     <Topbar text={'Home'} />
                     <div className="flex flex-col items-center lg:flex-row justify-evenly">
-                        <Box />
+                        <Box text='Feedback' value={countF} />
                         <Box />
                         <Box />
                     </div>
