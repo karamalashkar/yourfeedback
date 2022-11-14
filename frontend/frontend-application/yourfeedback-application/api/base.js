86b337863fd5f6de099689a26ed1428fda79474b
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-14 23:18:34 +0200

    Frontend <> Getting the token

--- frontend/frontend-application/yourfeedback-application/api/base.js
+++ frontend/frontend-application/yourfeedback-application/api/base.js
@@ -1,2 +1,12 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 //define the base url to be used
 export const baseURL = 'http://192.168.1.5:8000/api/v1/user';
+
+//get token from async storage
+const getToken = async()=>{
+    const token=await AsyncStorage.getItem('token');
+    return token;
+}
+
+const token=getToken();
