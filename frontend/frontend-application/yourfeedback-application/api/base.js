4690f8a5e6f240bb6af8cd8aef6f75c64a182898
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-14 23:25:30 +0200

    Frontend <> Importing axios

--- frontend/frontend-application/yourfeedback-application/api/base.js
+++ frontend/frontend-application/yourfeedback-application/api/base.js
@@ -1,4 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from "axios";
 
 //define the base url to be used
 export const baseURL = 'http://192.168.1.5:8000/api/v1/user';
