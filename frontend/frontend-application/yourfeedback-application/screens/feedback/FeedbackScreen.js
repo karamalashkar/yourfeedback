e25bc47d11cc1480f8176da70daeb427508d8ac7
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-15 01:05:33 +0200

    Frontend <> Getting feedback made by user

--- frontend/frontend-application/yourfeedback-application/screens/feedback/FeedbackScreen.js
+++ frontend/frontend-application/yourfeedback-application/screens/feedback/FeedbackScreen.js
@@ -1,16 +1,39 @@
 import { Image, ScrollView } from "react-native";
 import { styles } from "./style";
 import { FeedbackBox } from "../../components/box/Box";
+import { useEffect, useState } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getFeedback } from "../../api/getFeedback";
 
 const FeedbackScreen = () =>{
-    const data=true;
+    const [data,setData]=useState('')
+    const [feedback,setFeedback]=useState('')
+    useEffect(()=>{
+        const feedback=async () =>{
+            const business_id=await AsyncStorage.getItem('id');
+            const response=await getFeedback(business_id);
+            if(response.status=='failde'){
+                setData(false)
+                return null
+            }
+            setData(true)
+            setFeedback(response.data)
+        }
+
+        feedback();
+    },[])
+
     if (!data)
         return (
             <Image source={require('../../assets/data.png')} style={styles.image}/>
         )
     return(
         <ScrollView>
-            <FeedbackBox name='Market' date='DD-MM-YYYY'/>
+            {Object.values(feedback).map((feedback)=>{
+                return(
+                    <FeedbackBox name={feedback[0].business.name} date={feedback[0].created_at.substring(0,10)} />
+                )
+            })}
         </ScrollView>
     )
 }
