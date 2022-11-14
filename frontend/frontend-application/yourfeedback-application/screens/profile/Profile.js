91d569880191b2337e641b600a53c15e973ff6b6
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-15 00:28:26 +0200

    Frontend <> Getting number of feedback for the user

--- frontend/frontend-application/yourfeedback-application/screens/profile/Profile.js
+++ frontend/frontend-application/yourfeedback-application/screens/profile/Profile.js
@@ -3,11 +3,13 @@ import { ProfileBox } from "../../components/box/Box";
 import { styles } from "./style";
 import Button from "../../components/button/Button";
 import { getUser } from "../../api/getUser";
+import { countFeedback } from "../../api/countFeedback";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useEffect, useState } from "react";
 
 const ProfileScreen = () =>{
     const [name,setName]=useState('')
+    const [feedback,setFeedback]=useState('')
     useEffect(()=>{
         const user = async () =>{
             const business_id=await AsyncStorage.getItem('id');
@@ -16,15 +18,23 @@ const ProfileScreen = () =>{
                 setName(response.data.name)
             }
         }
+        const getCountFeedback=async () =>{
+            const business_id=await AsyncStorage.getItem('id');
+            const response=await countFeedback(business_id);
+            if(response.status=='success'){
+                setFeedback(response.data)
+            }
+        }
 
         user();
+        getCountFeedback();
     },[])
     
     return(
         <View style={styles.profile}>
             <Text style={styles.user}>{name}</Text>
             <View style={styles.box}>
-                <ProfileBox name='Feedback' value='12' />
+                <ProfileBox name='Feedback' value={feedback} />
                 <ProfileBox name='Discount' value='7' />
             </View>
             <Button text='Edit Profile'/>
