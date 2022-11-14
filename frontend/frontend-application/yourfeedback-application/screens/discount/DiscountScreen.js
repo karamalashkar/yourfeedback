f77bc38060461f311300873c997a18c734bf8278
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-15 01:19:14 +0200

    Frontend <> Getting discount by user

--- frontend/frontend-application/yourfeedback-application/screens/discount/DiscountScreen.js
+++ frontend/frontend-application/yourfeedback-application/screens/discount/DiscountScreen.js
@@ -1,16 +1,39 @@
 import { Image, ScrollView } from "react-native";
 import { styles } from "./style";
 import { DiscountBox } from "../../components/box/Box";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getDiscount } from "../../api/getDiscount";
+import { useState,useEffect } from "react";
 
 const DiscountScreen = () =>{
-    const data=true;
-    if (data)
+    const [data,setData]=useState('')
+    const [discount,setDiscount]=useState('')
+    useEffect(()=>{
+        const discount=async () =>{
+            const business_id=await AsyncStorage.getItem('id');
+            const response=await getDiscount(business_id);
+            if(response.status=='failde'){
+                setData(false)
+                return null
+            }
+            setData(true)
+            setDiscount(response.data)
+        }
+
+        discount();
+    },[])
+
+    if (!data)
         return (
             <Image source={require('../../assets/data.png')} style={styles.image}/>
         )
     return(
         <ScrollView>
-            <DiscountBox name='Market' value='12%' code='12345' />
+            {Object.values(discount).map((discount)=>{
+                return(
+                    <DiscountBox name={discount.business.name} value={`${discount.value}%`} code={discount.code} />
+                )
+            })}
         </ScrollView>
     )
 }
