faa18cbe4f2e659fe7689b2948f22111201603d5
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-14 23:09:56 +0200

    Frontend <> Checking login api response

--- frontend/frontend-application/yourfeedback-application/screens/login/LoginScreen.js
+++ frontend/frontend-application/yourfeedback-application/screens/login/LoginScreen.js
@@ -4,6 +4,8 @@ import { useNavigation } from '@react-navigation/native';
 import Input from "../../components/input/Input";
 import Button from "../../components/button/Button";
 import { useState } from "react";
+import { login } from "../../api/login";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const LoginScreen = () =>{
     const navigation=useNavigation();
@@ -11,12 +13,19 @@ const LoginScreen = () =>{
     const [password,setPassword]=useState('')
     const [errorMessage,setErrorMessage]=useState('')
 
-    const login = () =>{
+    const loginUser = async() =>{
         if(!email || !password){
             setErrorMessage('Enter all required fields')
         }
         else{
-            setErrorMessage('')
+            const post={email,password};
+            const response=await login(post);
+            if(response.response=='error'){
+                setErrorMessage('Invalid Credentials');
+                return null                        
+            }
+            AsyncStorage.setItem('id',response.user.id)
+            AsyncStorage.setItem('token',response.result.token)
         }
     }
 
@@ -27,7 +36,7 @@ const LoginScreen = () =>{
             <Text style={styles.error}>{errorMessage}</Text>
             <Input placeholder='Email' state={false} setValue={setEmail} setError={setErrorMessage}/>
             <Input placeholder='Password' state={true} setValue={setPassword} setError={setErrorMessage}/>
-            <Button text='Login' onPress={login}/>
+            <Button text='Login' onPress={loginUser}/>
             <Pressable onPress={()=>navigation.push('Register')}>
                 <Text>Don't have an account? Join Now</Text>
             </Pressable>
