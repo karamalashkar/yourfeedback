365a6e05f5f7646a3b805d89480de371a26f54ee
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-15 10:55:17 +0200

    Backend <> Adding check if user can make a feedback route

--- backend/yourfeedback-server/routes/api.php
+++ backend/yourfeedback-server/routes/api.php
@@ -7,41 +7,43 @@ use App\Http\Controllers\BusinessController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AuthBusinessController;
 
-Route::group(["prefix"=> "user"], function(){
-    Route::group(["middleware" => "auth:api"], function(){
-        Route::get('/user/{id}',[UserController::class,'getUser']);
-        Route::get('/question_category/{id}',[UserController::class,'questionByCategory']);
-        Route::post('/discount',[UserController::class,'discount']);
-        Route::get('/discount/{id}',[UserController::class,'getDiscount']);
-        Route::get('/count_discount/{id}',[UserController::class,'countDiscount']);
-        Route::post('/feedback',[UserController::class,'feedback']);
-        Route::get('/feedback/{id}',[UserController::class,'getFeedback']);
-        Route::get('/count_feedback/{id}',[UserController::class,'countFeedback']);
-        Route::get('/business_category/{id}',[BusinessController::class,'businessByCategory']);
-        Route::get('/near_business/{latitude}/{longitude}',[BusinessController::class,'nearBusiness']);
-        Route::get('/search_business/{word}',[BusinessController::class,'searchBusiness']);
+Route::group(["prefix"=> "v1"], function(){
+    Route::group(["prefix"=> "user"], function(){
+        Route::group(["middleware" => "auth:api"], function(){
+            Route::get('/{id}',[UserController::class,'getUser']);
+            Route::get('/question_category/{id}',[UserController::class,'questionByCategory']);
+            Route::post('/discount',[UserController::class,'discount']);
+            Route::get('/discount/{id}',[UserController::class,'getDiscount']);
+            Route::get('/count_discount/{id}',[UserController::class,'countDiscount']);
+            Route::post('/feedback',[UserController::class,'feedback']);
+            Route::get('/feedback/{id}',[UserController::class,'getFeedback']);
+            Route::get('/count_feedback/{id}',[UserController::class,'countFeedback']);
+            Route::get('/business_category/{id}',[BusinessController::class,'businessByCategory']);
+            Route::get('/near_business/{latitude}/{longitude}',[BusinessController::class,'nearBusiness']);
+            Route::get('/search_business/{word}',[BusinessController::class,'searchBusiness']);
+            Route::get('/make_feedback/{id}/{business_id}',[BusinessController::class,'canMakeFeedback']);
+        });
     });
+
+    Route::group(["prefix"=> "business"], function(){
+        Route::group(["middleware" => "auth:business"], function(){
+            Route::get('/{id}',[BusinessController::class,'getBusiness']);
+            Route::post('/edit',[BusinessController::class,'editBusiness']);
+            Route::get('/count_discount/{id}',[BusinessController::class,'countDiscount']);
+            Route::get('/count_feedback/{id}',[BusinessController::class,'countFeedback']);
+            Route::get('/feedback/{id}',[BusinessController::class,'getFeedback']);        
+            Route::get('/discount/{id}',[BusinessController::class,'getDiscount']);
+            Route::get('/discount_week/{id}',[BusinessController::class,'discountWeek']);
+            Route::get('/discount_month/{id}',[BusinessController::class,'discountMonth']);
+            Route::get('/discount_year/{id}',[BusinessController::class,'discountYear']);
+            Route::get('/feedback_week/{id}',[BusinessController::class,'feedbackWeek']); 
+            Route::get('/feedback_month/{id}',[BusinessController::class,'feedbackMonth']);
+            Route::get('/feedback_year/{id}',[BusinessController::class,'feedbackYear']);
+        });        
+    });
+
+    Route::post('/user',[UserController::class,'addUser']);
+    Route::post('/business',[BusinessController::class,'addBusiness']);
+    Route::post('/login',[AuthController::class,'login']);
+    Route::post('/business_login',[AuthController::class,'businessLogin']);
 });
-
-Route::group(["prefix"=> "business"], function(){
-    Route::group(["middleware" => "auth:business"], function(){
-        Route::get('/{id}',[BusinessController::class,'getBusiness']);
-        Route::post('/edit',[BusinessController::class,'editBusiness']);
-        Route::get('/count_discount/{id}',[BusinessController::class,'countDiscount']);
-        Route::get('/count_feedback/{id}',[BusinessController::class,'countFeedback']);
-        Route::get('/feedback/{id}',[BusinessController::class,'getFeedback']);        
-        Route::get('/discount/{id}',[BusinessController::class,'getDiscount']);
-        Route::get('/discount_week/{id}',[BusinessController::class,'discountWeek']);
-        Route::get('/discount_month/{id}',[BusinessController::class,'discountMonth']);
-        Route::get('/discount_year/{id}',[BusinessController::class,'discountYear']);
-        Route::get('/feedback_week/{id}',[BusinessController::class,'feedbackWeek']); 
-        Route::get('/feedback_month/{id}',[BusinessController::class,'feedbackMonth']);
-        Route::get('/feedback_year/{id}',[BusinessController::class,'feedbackYear']);
-    });        
-});
-
-Route::post('/user',[UserController::class,'addUser']);
-Route::post('/business',[BusinessController::class,'addBusiness']);
-Route::post('/login',[AuthController::class,'login']);
-Route::post('/business_login',[AuthController::class,'businessLogin']);
-
