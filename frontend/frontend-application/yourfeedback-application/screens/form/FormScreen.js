6b360ae05715fe3dc4166233a579bfddda1287fa
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-15 13:43:25 +0200

    Frontend <> Getting questions according to business category

--- frontend/frontend-application/yourfeedback-application/screens/form/FormScreen.js
+++ frontend/frontend-application/yourfeedback-application/screens/form/FormScreen.js
@@ -1,13 +1,32 @@
 import { Pressable, ScrollView, Text, TextInput, View } from "react-native";
 import { styles } from "./style";
 import Question from "../../components/question/Question";
+import { getQuestion } from "../../api/getQuestion";
+import { useEffect, useState } from "react";
 
-const FormScreen = (props) =>{
+const FormScreen = ({route}) =>{
+    const businessId=route.params.id;
+    const categoryId=route.params.category_id;
+    const [question,setQuestion]=useState('')
+    useEffect(()=>{
+        const getFeedbackQuestion = async () =>{
+            const response=await getQuestion(categoryId);
+            setQuestion(response)
+        }
+
+        getFeedbackQuestion();
+    },[])
+    
+
     return(
         <View style={styles.form}>
-            <Text style={styles.marketName}>{props.name}</Text>
+            <Text style={styles.marketName}>{route.params.name}</Text>
             <ScrollView style={styles.content}>
-                <Question />
+                {Object.values(question).map((question,index)=>{
+                    return(
+                        <Question key={index} question={question.question} />    
+                    )
+                })}
                 <View style={styles.button}>
                     <Pressable style={styles.press}>
                         <Text style={styles.text}>Submit</Text>
