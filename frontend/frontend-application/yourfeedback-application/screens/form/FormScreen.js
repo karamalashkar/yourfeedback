31ecc8a1e7e2c119d89138812527099d73bfff64
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-17 11:21:16 +0200

    Frontend <> Getting all answers entered by user in feedback form screen

--- frontend/frontend-application/yourfeedback-application/screens/form/FormScreen.js
+++ frontend/frontend-application/yourfeedback-application/screens/form/FormScreen.js
@@ -8,6 +8,12 @@ const FormScreen = ({route}) =>{
     const businessId=route.params.id;
     const categoryId=route.params.category_id;
     const [question,setQuestion]=useState('')
+    const [answerOne,setAnswerOne]=useState('')
+    const [answerTwo,setAnswerTwo]=useState('')
+    const [answerThree,setAnswerThree]=useState('')
+    const [answerFour,setAnswerFour]=useState('')
+    const [answerFive,setAnswerFive]=useState('')
+    const [errorMessage,setErrorMessage]=useState('')
     const result={}
     useEffect(()=>{
         const getFeedbackQuestion = async () =>{
@@ -28,17 +34,31 @@ const FormScreen = ({route}) =>{
         return null
     }
 
+    const submitFeedback = () =>{
+        if(!answerOne || !answerTwo || !answerThree || !answerFour || !answerFive){
+            setErrorMessage('Fill all required responses')
+            return null
+        }
+        //getting all entered responses
+        result[0].response=answerOne
+        result[1].response=answerTwo
+        result[2].response=answerThree
+        result[3].response=answerFour
+        result[4].response=answerFive
+    }
+
     return(
         <View style={styles.form}>
             <Text style={styles.marketName}>{route.params.name}</Text>
+            <Text>{errorMessage}</Text>
             <ScrollView style={styles.content}>
-                <Question question={result[0].question} />
-                <Question question={result[1].question} />
-                <Question question={result[2].question} />
-                <Question question={result[3].question} />
-                <Question question={result[4].question} />    
+                <Question question={result[0].question} setValue={setAnswerOne}/>
+                <Question question={result[1].question} setValue={setAnswerTwo}/>
+                <Question question={result[2].question} setValue={setAnswerThree}/>
+                <Question question={result[3].question} setValue={setAnswerFour}/>
+                <Question question={result[4].question} setValue={setAnswerFive}/>    
                 <View style={styles.button}>
-                    <Pressable style={styles.press}>
+                    <Pressable style={styles.press} onPress={submitFeedback}>
                         <Text style={styles.text}>Submit</Text>
                     </Pressable>
                 </View>     
