3eae83eb975cb0f6cb88467d4611c2e936102ca0
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-17 19:42:48 +0200

    Frontend <> Editing login screen style

--- frontend/frontend-application/yourfeedback-application/screens/login/style.js
+++ frontend/frontend-application/yourfeedback-application/screens/login/style.js
@@ -3,7 +3,13 @@ import { colors } from "../../constants/colors";
 
 export const styles=StyleSheet.create({
     component:{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%'
+    },
+    body:{
         width: '100%',
+        height: '92%',
         alignItems: 'center'
     },
     image:{
@@ -12,12 +18,25 @@ export const styles=StyleSheet.create({
         borderRadius: 10,
     },
     text:{
+        margin: 20,
         fontSize: 32,
         fontWeight: 'bold',
-        margin: 20,
         textAlign: 'center',
     },
-    error:{
+    errorMessage:{
         color: colors.red
+    },
+    footer:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: colors.red,
+    },
+    footerText:{
+        color: colors.white
     }
 })
