757eb92269a5b8c49ec5fcbbf6a0c73a39b91c32
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-18 01:23:39 +0200

    Frontend <> Adding update user slice

--- frontend/frontend-application/yourfeedback-application/redux/Slices/userSlice.js
+++ frontend/frontend-application/yourfeedback-application/redux/Slices/userSlice.js
@@ -1,5 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
 const initialState = {
     user:{},
     token:'',
 }
+export const userSlice = createSlice({
+    name:"user",
+    initialState,
+    reducers:{
+        updateUser :(state, action)=>{
+            state.user = action.payload.user
+        },    
+      }
+  }
+)
+export const {updateUser} = userSlice.actions
+export default userSlice.reducer
 
