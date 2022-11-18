32b601ae9b3d59e6d36523acec90ff61c7314343
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-18 13:10:45 +0200

    Frontend <> Linking get number of feedback per month api

--- frontend/frontend-website/yourfeedback-website/src/api/periodeFeedback.js
+++ frontend/frontend-website/yourfeedback-website/src/api/periodeFeedback.js
@@ -4,4 +4,6 @@ import { baseURL } from "./base";
 export const feedbackPeriode = async(id)=>{
     const resultWeek =await axios.get(`${baseURL}/feedback_week/${id}`);
     const weekCount=resultWeek.data.data;
+    const resultMonth = await axios.get(`${baseURL}/feedback_month/${id}`);
+    const monthCount=resultMonth.data.data;
 }
