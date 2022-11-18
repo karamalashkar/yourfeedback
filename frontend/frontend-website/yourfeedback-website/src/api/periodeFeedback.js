a50f51cbbcc145dfdb78926d4e225ec58e818ad9
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-18 13:12:30 +0200

    Frontend <> Linking get number of feedback per year api

--- frontend/frontend-website/yourfeedback-website/src/api/periodeFeedback.js
+++ frontend/frontend-website/yourfeedback-website/src/api/periodeFeedback.js
@@ -6,4 +6,6 @@ export const feedbackPeriode = async(id)=>{
     const weekCount=resultWeek.data.data;
     const resultMonth = await axios.get(`${baseURL}/feedback_month/${id}`);
     const monthCount=resultMonth.data.data;
+    const resultYear = await axios.get(`${baseURL}/feedback_year/${id}`);
+    const yearCount=resultYear.data.data;
 }
