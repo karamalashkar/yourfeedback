67792b5ef5063253c160c802bd1b275320517929
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-18 23:26:22 +0200

    Frontend <> Editing the main component

--- frontend/frontend-application/yourfeedback-application/App.js
+++ frontend/frontend-application/yourfeedback-application/App.js
@@ -1,8 +1,15 @@
-import { BottomTabNavigation } from './navigations/BottomTabNavigation';
+import StackSwitcher from './navigations/StackSwitcher';
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from './redux/Store';
+import { Provider } from 'react-redux';
 
 export default function App() {
   return (
-    <BottomTabNavigation />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <StackSwitcher />
+      </PersistGate>
+    </Provider>
   );
 }
 
