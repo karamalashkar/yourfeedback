a5dc6d184035297827bbaf523c364d41ae11caca
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-18 07:45:11 +0200

    Frontend <> Editing get discount link

--- frontend/frontend-application/yourfeedback-application/api/getDiscount.js
+++ frontend/frontend-application/yourfeedback-application/api/getDiscount.js
@@ -1,10 +1,10 @@
-import axios from "axios";
-import { baseURL } from "./base";
+import axiosInstance from "./base";
+import { getToken } from "../utilities/getToken";
 
 //get feedback by user id
 export const getDiscount = async(id)=>{
     try{
-        const result=await axios.get(`${baseURL}/discount/${id}`);
+        const result=await axiosInstance(getToken()).get(`/discount/${id}`);
         return result.data;
     }catch(error){
         console.log(error)
