50abd69bcc07913c31485cb138e0e12716c2e1ce
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-18 11:04:27 +0200

    Frontend <> Getting businesses according to user input

--- frontend/frontend-application/yourfeedback-application/screens/search/SearchScreen.js
+++ frontend/frontend-application/yourfeedback-application/screens/search/SearchScreen.js
@@ -1,11 +1,18 @@
 import { TextInput, View } from "react-native";
 import { styles } from "./style";
 import { colors } from "../../constants/colors";
+import { searchBusinessByName } from "../../api/searchBusiness";
 
-const SearchScreen = () =>{    
+const SearchScreen = () =>{
+    const searchBusiness = async(text)=>{
+        const result=await searchBusinessByName(text);
+        console.log(result)
+    }
+
     return(
         <View style={styles.search} >
-            <TextInput placeholder="Search" style={styles.input} selectionColor={colors.red} />
+            <TextInput placeholder="Search" style={styles.input} selectionColor={colors.red}
+            onChangeText={ text => searchBusiness(text)} />
         </View>
     ) 
 }
