a0dabce04ef9c0f990f154e7bdb098b91a741709
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-18 16:12:48 +0200

    Frontend <> Editing base file

--- frontend/frontend-website/yourfeedback-website/src/api/base.js
+++ frontend/frontend-website/yourfeedback-website/src/api/base.js
@@ -1,17 +1,20 @@
-import axios from "axios";
-
-//get token from local storage
-const token =localStorage.getItem('token');
+import axios from 'axios';
 
 //define the base url to be used
-export const baseURL = 'http://127.0.0.1:8000/api/v1/business';
+const baseURL = 'http://192.168.1.5:8000/api/v1/business';
 
-//define headers 
-axios.defaults.headers = {
-    'Authorization': `Bearer ${token}`,
-    "Access-Control-Allow-Origin": "*",
-    'Access-Control-Allow-Headers':'Content-Type',
-    'Access-Control-Allow-Headers': '*',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-    'Access-Control-Allow-Credentials': true
-}
+export default(token)=>{
+    return axios.create(
+        {
+            baseURL: baseURL,
+            headers:{
+                'Authorization':  `Bearer ${token}`,
+                "Access-Control-Allow-Origin": "*",
+                'Access-Control-Allow-Headers': "*",
+                'Access-Control-Allow-Methods': "GET, POST, PUT, DELETE",
+                'Access-Control-Allow-Credentials': true,
+                'Content-Type': "multipart/form-data",
+                'Accept': "application/json",
+            }
+        }
+    )}        
