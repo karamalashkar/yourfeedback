97fb929922bad0f71fc2b922ce37e09b856261b5
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-18 19:53:17 +0200

    Frontend <> Editing ip address used in login request

--- frontend/frontend-website/yourfeedback-website/src/api/login.js
+++ frontend/frontend-website/yourfeedback-website/src/api/login.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 //make http request to check email and password before login
 export const login = async (data)=>{
-    const result = await axios.post('http://127.0.0.1:8000/api/v1/business_login',data);
+    const result = await axios.post('http://192.168.1.5:8000/api/v1/business_login',data);
     return result.data;
 }
