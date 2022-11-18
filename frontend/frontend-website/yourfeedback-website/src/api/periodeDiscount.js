ef4b27a65459d7a1561a5cfb9172e4c1001315d4
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-18 13:26:20 +0200

    Frontend <> Linking get number of discount per month api

--- frontend/frontend-website/yourfeedback-website/src/api/periodeDiscount.js
+++ frontend/frontend-website/yourfeedback-website/src/api/periodeDiscount.js
@@ -4,4 +4,6 @@ import { baseURL } from "./base";
 export const discountPeriode = async(id)=>{
     const resultWeek =await axios.get(`${baseURL}/discount_week/${id}`);
     const weekCount=resultWeek.data.data;
+    const resultMonth = await axios.get(`${baseURL}/discount_month/${id}`);
+    const monthCount=resultMonth.data.data;
 }
