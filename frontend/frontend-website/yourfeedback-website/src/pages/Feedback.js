c2fd39e2325a2cf997bd315727ebd03e472b81bf
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-18 19:47:23 +0200

    Frontend <> Adding search option in feedback page

--- frontend/frontend-website/yourfeedback-website/src/pages/Feedback.js
+++ frontend/frontend-website/yourfeedback-website/src/pages/Feedback.js
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 const Feedback = ()=>{
     const business_id=localStorage.getItem('id');
     const [data,setData]=useState('')
+    const [name,setName]=useState('')
 
     useEffect(()=>{
         const feedback = async ()=>{
@@ -23,7 +24,6 @@ const Feedback = ()=>{
                     <Sidebar />
                 </div>
                 <div className="w-4/5">
-                    <Topbar text={'Feedback'} />
                     <img src='data.png' className="w-full"/>
                 </div>
             </div>
@@ -37,16 +37,25 @@ const Feedback = ()=>{
                 </div>
                 <div className="w-4/5">
                     <Topbar text={'Feedback'} />
+                    <div className="w-full flex flex-col bg-red mb-8 lg:flex-row">
+                        <input type='text' placeholder="Name" className="ml-16 p-4 border-2 border-gray-300 outline-0" 
+                        onChange={e => setName(e.target.value)}/>
+                    </div>
                     <div className="flex flex-wrap justify-evenly">
-                        {Object.values(data).map((answer)=>{
-                            console.log(answer)
+                        {Object.values(data).filter((value)=>{
+                            if(name==''){
+                                return value
+                            }else if(value[0].user.name==name){
+                                return value
+                            }
+                        }).map((answer)=>{
                             return(
                                 <Card name={answer[0].user.name} 
                                 date={answer[0].created_at.substring(0,10)} 
                                 data={answer} />
-                            )
+                            );
                         })}
-                    </div>
+                    </div>      
                 </div>
             </div>
         </>
