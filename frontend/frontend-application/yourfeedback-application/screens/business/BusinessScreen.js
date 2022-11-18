fbcd795ea074b3db979331c7ff61aa5ce3f9e334
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-18 03:21:46 +0200

    Frontend <> Editing business screen

--- frontend/frontend-application/yourfeedback-application/screens/business/BusinessScreen.js
+++ frontend/frontend-application/yourfeedback-application/screens/business/BusinessScreen.js
@@ -2,10 +2,10 @@ import { Image, Text, View, Alert } from "react-native";
 import { styles } from "./style";
 import { useNavigation } from '@react-navigation/native';
 import Button from "../../components/button/Button";
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useState } from "react";
 import Popup from "../../components/popup/Popup";
 import { canMakeFeedback } from "../../api/canMakeFeedback";
+import { store } from "../../redux/Store";
 
 const BusinessScreen = ({route}) =>{
     const navigation=useNavigation();
@@ -23,14 +23,14 @@ const BusinessScreen = ({route}) =>{
                 setError('Wrong Code')
                 return null
             }
-            const userId=await AsyncStorage.getItem('id');
+            let userId=store.getState().user.id
             const response=await canMakeFeedback(userId,businessId);
             if(response.status=='success'){
                 if(response.data.length!=0){
                     setError('You can make one feedback by month')
                 }
                 else{
-                    navigation.push('Form',{
+                    navigation.push('Feedback-Form',{
                         id: businessId,
                         name: route.params.name,
                         category_id: route.params.category_id
@@ -48,7 +48,7 @@ const BusinessScreen = ({route}) =>{
 
     return(
         <View>
-            <Image source={route.params.image} style={styles.image} />
+            <Image source={{uri: route.params.image}} style={styles.image} />
             <View style={styles.content}>
                 <Text style={styles.name}>{route.params.name}</Text>
                 <Text style={styles.body}>{route.params.bio}</Text>
