96df2267262142d2f254e625bf98a7771fc77064
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-18 19:48:15 +0200

    Frontend <> Editing discount box component

--- frontend/frontend-website/yourfeedback-website/src/components/Discountbox.js
+++ frontend/frontend-website/yourfeedback-website/src/components/Discountbox.js
@@ -1,9 +1,10 @@
 const Discountbox = (props)=>{
     return(
-        <div className="flex flex-col items-center justify-around h-44 mx-4 mb-8 bg-white rounded-xl shadow-2xl w-3/4 sm:w-2/4 lg:w-1/4">
-            <h1 className="text-2xl font-bold">{props.name}</h1>
-            <h1 className="text-2xl font-bold">{props.value}%</h1>
-            <h1 className="text-xl font-bold">{props.code}</h1>
+        <div className="flex flex-col items-start justify-around h-48 mx-4 mb-8 bg-white rounded-xl shadow-2xl w-3/4 sm:w-2/4 lg:w-1/4">
+            <h1 className="text-2xl font-bold ml-8">{props.code}</h1>
+            <h1 className="text-xl font-bold ml-8">{props.name}</h1>
+            <h1 className="text-2xl font-bold ml-8">{props.value}%</h1>
+            <h1 className="text-xl font-bold ml-8">{props.use}</h1>
         </div>    
     )
 }
