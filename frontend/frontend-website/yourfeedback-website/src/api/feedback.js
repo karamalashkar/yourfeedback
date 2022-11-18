3a3032cd5e9b9a732ee029aad97d8df74ea688e4
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-18 16:06:40 +0200

    Frontend <> Editing get all feedback link

--- frontend/frontend-website/yourfeedback-website/src/api/feedback.js
+++ frontend/frontend-website/yourfeedback-website/src/api/feedback.js
@@ -1,8 +1,8 @@
-import axios from "axios";
-import { baseURL } from "./base";
+import axiosInstance from "./base";
+import { getToken } from "../utilities/getToken";
 
 //getting feedback forms
 export const getFeedback = async (id)=>{
-    const result = await axios.get(`${baseURL}/feedback/${id}`);
+    const result = await axiosInstance(getToken()).get(`/feedback/${id}`);
     return result.data.data;
 }
