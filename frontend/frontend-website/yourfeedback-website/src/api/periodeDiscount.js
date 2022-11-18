61447501bf9134f3449a2aac2b6828fcded90606
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-18 13:28:39 +0200

    Frontend <> Linking get number of discount per year api

--- frontend/frontend-website/yourfeedback-website/src/api/periodeDiscount.js
+++ frontend/frontend-website/yourfeedback-website/src/api/periodeDiscount.js
@@ -6,4 +6,6 @@ export const discountPeriode = async(id)=>{
     const weekCount=resultWeek.data.data;
     const resultMonth = await axios.get(`${baseURL}/discount_month/${id}`);
     const monthCount=resultMonth.data.data;
+    const resultYear = await axios.get(`${baseURL}/discount_year/${id}`);
+    const yearCount=resultYear.data.data;
 }
