ead46c382e3386577a824050e9640480b52429cf
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-18 03:10:07 +0200

    Frontend <> Editing ad question link

--- frontend/frontend-application/yourfeedback-application/api/addFeedback.js
+++ frontend/frontend-application/yourfeedback-application/api/addFeedback.js
@@ -1,12 +1,13 @@
 import axios from "axios";
-import { baseURL } from "./base";
+import axiosInstance from "./base";
+import { getToken } from "../utilities/getToken";
 const date=new Date();
 
 //adding feedback survey
 export const addFeedback = async(userId,businessId,data)=>{
     try{
         {Object.values(data).map(async(data)=>{
-                const result=await axios.post(`${baseURL}/feedback`,{
+                const result=await axiosInstance(getToken()).post(`/feedback`,{
                     user_id:userId,
                     business_id:businessId,
                     survey_id:`${userId}${businessId}${date.getDate()}${date.getMonth()}`,
