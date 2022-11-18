257f2445641b100d6d19eabee398f35a03b96839
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-18 02:03:41 +0200

    Frontend <> Editing get user link

--- frontend/frontend-application/yourfeedback-application/api/getUser.js
+++ frontend/frontend-application/yourfeedback-application/api/getUser.js
@@ -1,13 +1,13 @@
-import axios from "axios";
-import { baseURL } from "./base";
+import axiosInstance from "./base";
+import { getToken } from "../utilities/getToken";
 
 //get user by id
 export const getUser = async(id)=>{
     try{
-        const result=await axios.get(`${baseURL}/${id}`);
+        const result=await axiosInstance(getToken()).get(`/${id}`);
         return result.data;
     }catch(error){
         console.log(error);
     }
-    
+   
 }
