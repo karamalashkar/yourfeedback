c729f2215d4691fb66927d2d32445d5c9ea46435
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-18 16:10:28 +0200

    Frontend <> Editing get number of feedback link

--- frontend/frontend-website/yourfeedback-website/src/api/countFeedback.js
+++ frontend/frontend-website/yourfeedback-website/src/api/countFeedback.js
@@ -1,8 +1,8 @@
-import axios from "axios";
-import { baseURL } from "./base";
+import axiosInstance from "./base";
+import { getToken } from "../utilities/getToken";
 
 //get total number of feedback by user id
 export const getCountFeedback = async (id)=>{
-    const result = await axios.get(`${baseURL}/count_feedback/${id}`);
+    const result = await axiosInstance(getToken()).get(`/count_feedback/${id}`);
     return result.data.data;
 }
