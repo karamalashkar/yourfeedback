242eab87f1f0fcf6093c323e90e731d42d5d24c2
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-18 10:22:42 +0200

    Frontend <> Adding search item component used in search screen

---
+++ frontend/frontend-application/yourfeedback-application/components/searchItem/SearchItem.js
@@ -0,0 +1,15 @@
+import { Image, Pressable, Text, View } from "react-native";
+import { styles } from "./style";
+
+const SearchItem = (props) =>{
+    return(
+        <Pressable style={styles.card} onPress={props.onPress}>
+            <View style={styles.content}>
+                <Image source={props.image} style={styles.image} />
+                <Text style={styles.text}>{props.name}</Text>
+            </View>    
+        </Pressable>
+    )
+}
+
+export default SearchItem;
