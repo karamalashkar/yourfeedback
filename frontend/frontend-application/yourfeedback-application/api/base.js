2c5168edcff6bcb89c78520201b80ac8940eb78c
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-18 02:27:56 +0200

    Frontend <> Editing base file containing base url and headers

--- frontend/frontend-application/yourfeedback-application/api/base.js
+++ frontend/frontend-application/yourfeedback-application/api/base.js
@@ -1,24 +1,20 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import axios from "axios";
+import axios from 'axios';
 
 //define the base url to be used
-export const baseURL = 'http://192.168.1.5:8000/api/v1/user';
+const baseURL = 'http://192.168.1.5:8000/api/v1/user';
 
-//get token from async storage
-const getToken = async()=>{
-    const token=await AsyncStorage.getItem('token');
-    return token;
-}
-
-const token=getToken();
-
-//define headers 
-axios.defaults.headers = {
-    'Authorization': `Bearer ${token}`,
-    "Access-Control-Allow-Origin": "*",
-    'Access-Control-Allow-Headers': '*',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-    'Access-Control-Allow-Credentials': true,
-    'Content-Type': "multipart/form-data",
-    'Accept': "application/json",
-}
+export default(token)=>{
+    return axios.create(
+        {
+            baseURL: baseURL,
+            headers:{
+                'Authorization':  `Bearer ${token}`,
+                "Access-Control-Allow-Origin": "*",
+                'Access-Control-Allow-Headers': "*",
+                'Access-Control-Allow-Methods': "GET, POST, PUT, DELETE",
+                'Access-Control-Allow-Credentials': true,
+                'Content-Type': "multipart/form-data",
+                'Accept': "application/json",
+            }
+        }
+    )}        
