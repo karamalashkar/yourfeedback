0201cc6239bf1a485f39627c7edff1af83ef3c3d
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-18 11:17:43 +0200

    Frontend <> Adding search results to search item component

--- frontend/frontend-application/yourfeedback-application/screens/search/SearchScreen.js
+++ frontend/frontend-application/yourfeedback-application/screens/search/SearchScreen.js
@@ -1,19 +1,51 @@
-import { TextInput, View } from "react-native";
+import { ScrollView, Text, TextInput, View } from "react-native";
 import { styles } from "./style";
 import { colors } from "../../constants/colors";
+import { useState } from "react";
 import { searchBusinessByName } from "../../api/searchBusiness";
+import SearchItem from "../../components/searchItem/SearchItem";
 
 const SearchScreen = () =>{
+    const [business,setBusiness]=useState('')
+    const [errorMessage,setErrorMessage]=useState('')
     const searchBusiness = async(text)=>{
+        if(!text){
+            setErrorMessage('')
+            setBusiness('')
+            return null
+        }
         const result=await searchBusinessByName(text);
-        console.log(result)
+        if(result.status=='failed'){
+            setErrorMessage('')
+            setBusiness('')
+        }
+
+        if(result.data.length==0){
+            setErrorMessage(`No results for ${text}`)
+            setBusiness('')
+            return null
+        }
+        setErrorMessage('')
+        setBusiness(result.data)
     }
 
     return(
-        <View style={styles.search} >
-            <TextInput placeholder="Search" style={styles.input} selectionColor={colors.red}
-            onChangeText={ text => searchBusiness(text)} />
-        </View>
+        <>
+        <View>
+            <View style={styles.search} >
+                <TextInput placeholder="Search" style={styles.input} selectionColor={colors.red}
+                onChangeText={ text => searchBusiness(text)} />
+            </View>
+            <Text style={styles.errorMessage}>{errorMessage}</Text>
+            <ScrollView>
+                {Object.values(business).map((business)=>{
+                    return(
+                        <SearchItem key={business.id} image={{uri:business.image}} name={business.name} />
+                    );
+                })}
+            </ScrollView>
+        </View>    
+        </>
     ) 
 }
 
