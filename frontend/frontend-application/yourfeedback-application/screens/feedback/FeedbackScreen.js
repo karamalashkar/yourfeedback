bac5c7651051dd3ba2058c15d6f63bf67d9448e6
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-25 23:25:08 +0200

    Frontend <> Editing feedback screen

--- frontend/frontend-application/yourfeedback-application/screens/feedback/FeedbackScreen.js
+++ frontend/frontend-application/yourfeedback-application/screens/feedback/FeedbackScreen.js
@@ -1,12 +1,11 @@
-import { RefreshControl, Image, ScrollView } from "react-native";
-import { styles } from "./style";
-import { FeedbackBox } from "../../components/box/Box";
+import { RefreshControl, Image, ScrollView, Text, View } from "react-native";
+import FeedbackCard from "../../components/feedbackcard/FeedbackCard";
 import { useEffect, useState } from "react";
 import { getFeedback } from "../../api/getFeedback";
 import { store } from "../../redux/Store";
+import EmptyState from "../../components/emptystate/EmptyState";
 
 const FeedbackScreen = () =>{
-    const [data,setData]=useState('')
     const [feedback,setFeedback]=useState('')
     const [load,setLoad]=useState(true)
 
@@ -14,10 +13,10 @@ const FeedbackScreen = () =>{
         let userId=store.getState().user.id
         const response=await getFeedback(userId);
         if(response.status=='failed' || response.data.length==0){
-            setData(false)
+            setFeedback('')
+            setLoad(false)
             return null
         }
-        setData(true)
         setFeedback(response.data)
         setLoad(false)
     }
@@ -26,19 +25,28 @@ const FeedbackScreen = () =>{
         userFeedback();
     },[])
 
-    if (!data)
-        return (
-            <Image source={require('../../assets/data.png')} style={styles.image}/>
+    if(!feedback){
+        return(
+            <ScrollView refreshControl={<RefreshControl 
+            refreshing={load} onRefresh={userFeedback}/>} >
+                <EmptyState text={'No Available Data'} />
+            </ScrollView>            
         )
+    }
+
     return(
-        <ScrollView refreshControl={<RefreshControl 
-        refreshing={load} onRefresh={userFeedback}/>} >
-            {Object.values(feedback).map((feedback,index)=>{
-                return(
-                    <FeedbackBox key={index} name={feedback[0].business.name} date={feedback[0].created_at.substring(0,10)} />
-                )
-            })}
-        </ScrollView>
+        <>
+            <ScrollView showsVerticalScrollIndicator={false} refreshControl={<RefreshControl 
+            refreshing={load} onRefresh={userFeedback}/>} >
+                {Object.values(feedback).map((feedback,index)=>{
+                    return(
+                        <FeedbackCard key={index} name={feedback[0].business.name} 
+                        date={feedback[0].created_at.substring(0,10)} 
+                        data={feedback}/>
+                    )
+                })}
+            </ScrollView>
+        </>
     )
 }
 
