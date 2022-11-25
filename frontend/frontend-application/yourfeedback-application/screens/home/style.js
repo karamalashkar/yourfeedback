607c49e29f8b259db84d2ed4b49561e7c55f123f
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-26 01:54:21 +0200

    Frontend <> Editing home screen style

--- frontend/frontend-application/yourfeedback-application/screens/home/style.js
+++ frontend/frontend-application/yourfeedback-application/screens/home/style.js
@@ -2,24 +2,14 @@ import { StyleSheet } from "react-native";
 import { colors } from "../../constants/colors";
 
 export const styles=StyleSheet.create({
+    title:{
+        margin: 16,
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
     scroll:{
         width: '100%',
         height: '90%'
-    },
-    image:{
-        width: '100%',
-        height: '100%'
-    },
-    search:{
-        width: '100%',
-        height: 50,
-        justifyContent: 'center',
-        borderBottomWidth: 3,
-        borderBottomColor: colors.grey
-    },
-    searchText:{
-        marginLeft: 15,
-        fontSize: 16
     },
     errorMessage:{
         marginTop: 10,
