26ba3f8727b47b7b5d9f03894345a3376ec040c2
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-25 23:04:52 +0200

    Frontend <> Editing popup component

--- frontend/frontend-application/yourfeedback-application/components/popup/Popup.js
+++ frontend/frontend-application/yourfeedback-application/components/popup/Popup.js
@@ -10,19 +10,20 @@ const Popup = (props) =>{
         <View style={styles.popup}>
             <View style={styles.content}>
                 <Text style={styles.text}>Enter feedback code</Text>
-                <Text style={styles.error}>{props.error}</Text>
-                
+                <Text style={styles.message}>Contact us to get your code</Text>
+
                 <TextInput placeholder="Code"
                 selectionColor={colors.red} 
                 style={styles.input} 
                 onChangeText={(text)=>props.setValue(text)} />
-                
+                <Text style={styles.error}>{props.error}</Text>
+                  
                 <View style={styles.button}>
-                    <Pressable style={styles.continueButton} onPress={props.continue}>
-                        <Text style={styles.buttonText}>Continue</Text>
-                    </Pressable>
                     <Pressable style={styles.backButton} onPress={props.back}>
                         <Text style={styles.buttonText}>Back</Text>
+                    </Pressable>
+                    <Pressable style={styles.continueButton} onPress={props.continue}>
+                        <Text style={styles.buttonText}>Continue</Text>
                     </Pressable>
                 </View>
             </View>
