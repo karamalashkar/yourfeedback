7c8f489cff2616958a2ba81635937b3ce1c3686b
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-25 23:08:45 +0200

    Frontend <> Editing question component

--- frontend/frontend-application/yourfeedback-application/components/question/Question.js
+++ frontend/frontend-application/yourfeedback-application/components/question/Question.js
@@ -5,15 +5,18 @@ import { colors } from "../../constants/colors";
 const Question = (props) =>{
     return(
         <View style={styles.component}>
-            <Text style={styles.question}>{props.question}</Text>
-            <TextInput 
-            style={styles.input} 
-            selectionColor={colors.red} 
-            multiline={true}
-            numberOfLines={3}
-            maxLength={150}
-            autoFocus={false}
-            onChangeText={(text)=>props.setValue(text)}/>
+            <View style={styles.content}>
+                <Text style={styles.question}>{props.question}</Text>
+                <TextInput
+                placeholder="Answer" 
+                style={styles.input} 
+                selectionColor={colors.red} 
+                multiline={true}
+                numberOfLines={3}
+                maxLength={150}
+                autoFocus={false}
+                onChangeText={(text)=>props.setValue(text)}/>
+            </View>
         </View>
     )
 }
