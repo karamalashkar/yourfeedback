7888088e662d062857e3d9700caf18e6ab9ba1e9
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-25 23:17:27 +0200

    Frontend <> Editing home stack navigation

--- frontend/frontend-application/yourfeedback-application/navigations/HomeStackNavigation.js
+++ frontend/frontend-application/yourfeedback-application/navigations/HomeStackNavigation.js
@@ -5,47 +5,53 @@ import HomeScreen from "../screens/home/HomeScreen";
 import BusinessScreen from "../screens/business/BusinessScreen";
 import FormScreen from "../screens/form/FormScreen";
 import SearchScreen from "../screens/search/SearchScreen";
+import Bar from "../components/bar/Bar";
+import { colors } from "../constants/colors";
 
 export function HomeStackNavigation() {
   const RootStackNav = createStackNavigator();
   const navigationRef = useRef(null);
 
   return (
-    <NavigationContainer
-    ref={navigationRef} 
-    independent={true} >
-      <RootStackNav.Navigator >
+    
+      <RootStackNav.Navigator 
+      backButtonTextStyle={{color: 'red'}}>
 
         <RootStackNav.Screen
           name="Home"
           component={HomeScreen}
-          options={{
-            headerShown: false
-          }}  
+          options={()=>({
+            headerStyle:{
+              backgroundColor: colors.red
+            },
+            headerTitle: () => <Bar text={'Home'}/>
+          })}  
         />
 
         <RootStackNav.Screen
           name="Business"
           component={BusinessScreen}
+          options={({route})=>({
+            headerTintColor: colors.white,
+            headerStyle:{
+              backgroundColor: colors.red
+            },
+            headerTitle: () => <Bar text={route.params.name}/>
+          })}
         />
 
         <RootStackNav.Screen
           name="Feedback-Form"
           component={FormScreen}
-          options={{
-            headerLeft: () => {}
-          }}  
-        />
-
-        <RootStackNav.Screen
-          name="Search"
-          component={SearchScreen}
-          options={{
-            headerShown: false
-          }}
+          options={({route})=>({
+            headerTintColor: colors.white,
+            headerStyle:{
+              backgroundColor: colors.red
+            },
+            headerTitle: () => <Bar text={route.params.name}/>
+          })}    
         />
 
       </RootStackNav.Navigator>
-    </NavigationContainer>
   );
 }
