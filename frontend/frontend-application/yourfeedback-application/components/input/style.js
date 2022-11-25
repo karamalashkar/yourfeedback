e92d2903ff70736ca184ab9e92cc2042451d3774
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-25 23:04:02 +0200

    Frontend <> Editing input component style

--- frontend/frontend-application/yourfeedback-application/components/input/style.js
+++ frontend/frontend-application/yourfeedback-application/components/input/style.js
@@ -3,18 +3,27 @@ import { colors } from "../../constants/colors"
 
 export const styles=StyleSheet.create({
     input:{
-        width: '90%',
+        width: '100%',
         height: 45,
         marginTop: 10,
         padding: 10,
-        borderWidth: 2,
         borderRadius: 10,
-        borderColor: 'black',
+        borderColor: 'grey',
         fontSize: 16,
+        backgroundColor: colors.white,
+        shadowColor: "#000",
+        shadowOffset: {
+	        width: 0,
+	        height: 2,
+        },
+        shadowOpacity: 0.32,
+        shadowRadius: 5.46,
+
+        elevation: 5,
     },
     error:{
         color: colors.red,
-        fontSize: 14
+        fontSize: 14,
     }
 })    
 
