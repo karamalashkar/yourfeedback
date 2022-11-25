b265e002470ef6687a15fe3023463866a08ec169
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-25 22:48:34 +0200

    Frontend <> Adding answer component style

---
+++ frontend/frontend-application/yourfeedback-application/components/answer/style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    component:{
+        marginBottom: 20
+    },
+    question:{
+        maxWidth: '90%',
+        marginLeft: 15,
+        fontSize: 16,
+    },
+    response:{
+        margin: 2,
+        marginLeft: 15,
+        fontSize: 16,
+        fontWeight: 'bold'
+    }
+})
