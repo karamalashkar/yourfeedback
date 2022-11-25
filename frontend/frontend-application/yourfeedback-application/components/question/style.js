72dc31a6de496221149c5ab6e7fed3f704381f30
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-25 23:09:20 +0200

    Frontend <> Editing question component style

--- frontend/frontend-application/yourfeedback-application/components/question/style.js
+++ frontend/frontend-application/yourfeedback-application/components/question/style.js
@@ -3,21 +3,27 @@ import { colors } from "../../constants/colors";
 
 export const styles=StyleSheet.create({
     component:{
-        width: '100%',
+        alignItems: 'center'
+    },
+    content:{
+        padding: 20,
+        width: '90%',
         alignItems: 'center',
         marginTop: 10,
-        marginBottom: 20
+        marginBottom: 20,
+        backgroundColor: colors.white,
+        borderRadius: 10
     },
     question:{
         fontSize: 18,
-        width: '70%'
+        width: '90%'
     },
     input:{
-        width: '70%',
+        width: '90%',
         fontSize: 14,
         marginTop: 5,
         outlineStyle: 'none',
-        borderBottomWidth: 2,
+        borderBottomWidth: 1,
         borderBottomColor: colors.grey
     },
 })
