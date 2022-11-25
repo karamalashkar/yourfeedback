a84bee20904af72eb3d0c3260445b19abfaa4360
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-25 22:51:57 +0200

    Frontend <> Editing box component

--- frontend/frontend-application/yourfeedback-application/components/box/Box.js
+++ frontend/frontend-application/yourfeedback-application/components/box/Box.js
@@ -1,38 +1,17 @@
 import { View, Text } from "react-native";
 import { styles } from "./style";
 
-const FeedbackBox = (props) =>{
-    return(
-        <View style={styles.component}>
-            <View style={styles.box}>
-                <Text style={styles.text}>{props.name}</Text>
-                <Text style={styles.text}>{props.date}</Text>
-            </View>
-        </View>
-    )
-}
-
-const DiscountBox = (props) =>{
-    return(
-        <View style={styles.component}>
-            <View style={styles.box}>
-                <Text style={styles.text}>{props.name}</Text>
-                <Text style={styles.text}>{props.value}</Text>
-                <Text style={styles.text}>{props.code}</Text>
-            </View>
-        </View>
-    )
-}
-
 const ProfileBox = (props) =>{
     return(
         <View style={styles.component}>
             <View style={styles.box}>
-                <Text style={styles.text}>{props.name}</Text>
-                <Text style={styles.text}>{props.value}</Text>
+                <View style={styles.head}>
+                    <Text style={styles.text}>{props.name}</Text>
+                </View>
+                <Text style={styles.value}>{props.value}</Text>
             </View>
         </View>
     )
 }
 
-export { FeedbackBox,DiscountBox,ProfileBox };
+export default ProfileBox;
