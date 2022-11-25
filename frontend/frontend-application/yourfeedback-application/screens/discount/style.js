c4a9734ddbd0b41fc779a5fafadf2307c18e4a29
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-25 23:24:20 +0200

    Frontend <> Editing discount screen style

--- frontend/frontend-application/yourfeedback-application/screens/discount/style.js
+++ frontend/frontend-application/yourfeedback-application/screens/discount/style.js
@@ -1,8 +1,7 @@
 import { StyleSheet } from "react-native";
 
 export const styles=StyleSheet.create({
-    image:{
-        width: '100%',
-        height: '100%'
-    }
+    scroll:{
+        marginTop: 20
+    },
 })
