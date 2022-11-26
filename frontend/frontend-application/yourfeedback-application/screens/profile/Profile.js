277e2b75e13d1d3c66186c7b3d7d88e644564df0
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-26 02:00:09 +0200

    Frontend <> Editing profile screen

--- frontend/frontend-application/yourfeedback-application/screens/profile/Profile.js
+++ frontend/frontend-application/yourfeedback-application/screens/profile/Profile.js
@@ -1,5 +1,5 @@
 import { RefreshControl, ScrollView, Text, View } from "react-native";
-import { ProfileBox } from "../../components/box/Box";
+import ProfileBox from "../../components/box/Box";
 import { styles } from "./style";
 import Button from "../../components/button/Button";
 import { getUser } from "../../api/getUser";
@@ -8,11 +8,16 @@ import { countDiscount } from "../../api/countDiscount";
 import { useEffect, useState } from "react";
 import { store } from "../../redux/Store";
 import {deleteUser} from "../../redux/Slices/userSlice";
+import ProfileCard from "../../components/profilecard/ProfileCard";
+import { recentFeedback } from "../../api/recentFeedback";
+import { recentDiscount } from "../../api/recentDiscount";
 
 const ProfileScreen = () =>{
     const [name,setName]=useState('')
     const [feedback,setFeedback]=useState('')
     const [discount,setDiscount]=useState('')
+    const[userRecentFeedback,setUserRecentFeedback]=useState('')
+    const[userRecentDiscount,setUserRecentDiscount]=useState('')
     const [load,setLoad]=useState(true)
 
     let userId=store.getState().user.id
@@ -22,17 +27,21 @@ const ProfileScreen = () =>{
             setName(response.data.name)
         }
     }
-    const userInfo=async () =>{
+    const userInfo = async () =>{
         const responseFeedback=await countFeedback(userId);
         const responseDiscount=await countDiscount(userId);
-            setFeedback(responseFeedback.data)
-            setDiscount(responseDiscount.data)
-            setLoad(false)
+        const responseRecentFeedback=await recentFeedback(userId);
+        const responseRecentDiscount=await recentDiscount(userId)    
+        setFeedback(responseFeedback.data)
+        setDiscount(responseDiscount.data)
+        setUserRecentFeedback(responseRecentFeedback)
+        setUserRecentDiscount(responseRecentDiscount)
+        setLoad(false)
     }
     
     useEffect(()=>{
-        user();
-        userInfo()    
+        user()
+        userInfo()
     },[])
     
     const logout = ()=>{
@@ -42,12 +51,22 @@ const ProfileScreen = () =>{
     return(
         <View style={styles.profile}>
             <Text style={styles.user}>{name}</Text>
-            <ScrollView style={styles.box} refreshControl={<RefreshControl 
+            <ScrollView style={styles.scroll} refreshControl={<RefreshControl 
             refreshing={load} onRefresh={userInfo}/>}>
-                <ProfileBox name='Feedback' value={feedback} />
-                <ProfileBox name='Discount' value={discount} />
+                <View style={styles.box}>
+                    <ProfileBox name='Feedback' value={feedback} />
+                    <ProfileBox name='Discount' value={discount} />
+                </View>
+                <View style={styles.info}>
+                    <Text style={styles.title}>Recent Feedback</Text>
+                    <ProfileCard name={userRecentFeedback}/>
+                    <Text style={styles.title}>Recent Discount</Text>
+                    <ProfileCard name={userRecentDiscount} />
+                </View>
             </ScrollView>
-            <Button text='Logout' onPress={logout}/>
+            <View style={styles.button}>
+                <Button text='Logout' onPress={logout}/>
+            </View>    
         </View>
     )
 }
