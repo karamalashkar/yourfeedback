3f0245b786d3a1fd15e475b042081ec463a9fccc
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-26 15:34:07 +0200

    Frontend <> Editing landing page style

--- frontend/frontend-website/yourfeedback-website/src/pages/Landing.js
+++ frontend/frontend-website/yourfeedback-website/src/pages/Landing.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import Navbar from "../components/Navbar";
 import Input from "../components/Input";
 import Button from "../components/Button";
 import { login } from "../api/login";
@@ -31,24 +30,27 @@ const Landing = () =>{
     }
     
     return(
-        <>
-            <Navbar />
-            <div className='flex flex-col align-center xl:flex-row'>
-                <div className='xl:w-5/12 flex flex-col items-center'>
-                    <h1 className='mb-8 text-5xl font-bold text-center'>Give me your <br/> feedback</h1>
-                    <h2 className="my-4 text-lg text-red-700 font-bold">{error}</h2>
+        <div className="w-full h-full fixed bg-white flex items-center justify-center">
+            <div className='w-4/5 h-4/5 fixed bg-white flex flex-col align-center xl:flex-row drop-shadow-2xl rounded-lg'>
+                <div className="w-2/4 flex flex-col items-center justify-center">
+                    <h1 className="mb-8 font-bold text-3xl">Login</h1>
+                    <h2 className="text-md text-red-700">{error}</h2>
                     <form onSubmit={loginBusiness} className="w-full flex flex-col items-center">
                         <Input type='email' text='Email' setValue={setEmail} setError={setError} />
                         <Input type='password' text='Password' setValue={setPassword} setError={setError} />
                         <Button text='Sign In' />
                     </form>    
-                    <h4>Don't have an account? <a href='/signup'>Join Now</a></h4>
+                    <h4 className="my-2 text-sm font-semibold">Don't have an account? <a href='/signup' className="text-red-700">Join Now</a></h4>
                 </div>
-                <div className='my-4 flex justify-center xl:w-7/12'>
-                    <img src='feedback.jpg' className='mt-4 rounded-xl' alt='feedback' />
-                </div>    
-            </div>    
-        </>
+                <div className="w-2/4 h-full bg-red-700 flex flex-col items-center rounded-tr-lg rounded-br-lg">
+                    <img src='darkLogo.png' className='h-1/4 w-2/5 mt-20' alt='feedback' />
+                    <h1 className="text-white text-opacity-80 w-3/4 mt-8">
+                        Your feedback website is for business owner to start receiving feedback from their 
+                        customer's according to a feedback code.
+                    </h1>
+                </div>
+            </div>
+        </div>        
     )
 }
 
