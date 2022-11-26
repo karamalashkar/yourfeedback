4c71121c05c4266522a18b1dfa7618798b5236fe
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-26 15:45:53 +0200

    Frontend <> Editing select component

--- frontend/frontend-website/yourfeedback-website/src/components/Select.js
+++ frontend/frontend-website/yourfeedback-website/src/components/Select.js
@@ -4,11 +4,12 @@ const Select = (props)=>{
             <select
             onChange={e => props.setValue(e.target.value)}
             onFocus={() => props.setError('')}
-            className='w-1/2 p-3 my-2 border-2 border-black rounded-xl'> 
+            className='w-3/5 p-3 my-2 border border-gray-300 outline-none bg-white rounded-lg'> 
                 <option value={''}>Category</option>  
                 <option value={'1'}>Market</option>
                 <option value={'2'}>Medical Center</option>
                 <option value={'3'}>Gym</option>
+                <option value={'4'}>Restaurant</option>
             </select>
         </> 
     )
