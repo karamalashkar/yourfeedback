27459d69fa95b410f4d23621dc6bba1cc7ea509c
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-26 16:00:40 +0200

    Frontend <> Editing top bar component style

--- frontend/frontend-website/yourfeedback-website/src/components/Topbar.js
+++ frontend/frontend-website/yourfeedback-website/src/components/Topbar.js
@@ -1,7 +1,7 @@
 const Topbar = (props) =>{
     return(
         <div className="flex item-start w-4/5 h-20">
-            <h1 className="text-4xl font-bold mx-16 my-4">{props.text}</h1>
+            <h1 className="text-3xl font-bold mx-16 my-4">{props.text}</h1>
         </div>
     )
 }
