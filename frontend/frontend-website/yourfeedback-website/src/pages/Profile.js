078497b73df1b47d5ec9dae258cf5079e81be2db
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-26 16:37:12 +0200

    Frontend <> Editing profile page

--- frontend/frontend-website/yourfeedback-website/src/pages/Profile.js
+++ frontend/frontend-website/yourfeedback-website/src/pages/Profile.js
@@ -9,6 +9,7 @@ const Profile = ()=>{
     const [image,setImage]=useState('market.jpg')
     const [name,setName]=useState('')
     const [bio,setBio]=useState('')
+    const [location,setLocation]=useState('')
     const [feedbackCode,setFeedbackCode]=useState('')
     const [isOpen,setOpen]=useState(false)
     useEffect(()=>{
@@ -16,6 +17,7 @@ const Profile = ()=>{
             const result=await getBusiness(business_id);
             setName(result.name)
             setBio(result.bio)
+            setLocation(result.location)
             setFeedbackCode(result.feedback_code)
             if(result.image){
                 setImage(result.image)
@@ -34,15 +36,16 @@ const Profile = ()=>{
                 <div className="w-4/5">
                     <Topbar text={'Profile'} />
                     <Editprofile open={isOpen} onClose={()=>setOpen(false)} />
-                    <div className="flex flex-col items-center lg:flex-row my-12">
-                        <div className="flex justify-center w-full xl:w-2/4">
-                            <img src={image} alt='profile picture' className="w-3/4 h-96 rounded-xl shadow-2xl" />
-                        </div>
-                        <div className="flex-col items-start w-3/4 mt-4 xl:w-2/4">
-                            <h1 className="text-4xl font-bold">{name}</h1>
-                            <p className="mt-8 max-w-md h-auto text-lg">{bio}</p>
-                            <h2 className='my-8 text-lg'>Feedback Code: <span className="font-bold">{feedbackCode}</span></h2>
-                            <button onClick={()=>setOpen(true)} className='w-1/2 p-3 mt-4 bg-red-700 text-white rounded-xl'>Edit Profile</button>    
+                    <div className="w-full h-auto flex items-center justify-center">
+                        <div className="h-auto w-4/5 flex flex-col rounded-tr-xl rounded-br-xl lg:flex-row my-12 items-center bg-white">
+                            <img src={image} alt='profile picture' className="w-2/4 h-96 rounded-tl-xl rounded-bl-xl shadow-2xl" />
+                            <div className="ml-8 flex-col items-start xl:w-2/4 bg-white">
+                                <h1 className="text-3xl font-bold">{name}</h1>
+                                <p className="mt-8 max-w-md h-auto text-lg">{bio}</p>
+                                <h2 className='my-4 text-lg'>Location: <span className="font-bold">{location}</span></h2>
+                                <h2 className='my-4 text-lg'>Feedback Code: <span className="font-bold">{feedbackCode}</span></h2>
+                                <button onClick={()=>setOpen(true)} className='w-1/2 p-3 mt-4 bg-red-700 text-white font-bold rounded-xl'>Edit Profile</button>    
+                            </div>
                         </div>
                     </div>
                 </div>
