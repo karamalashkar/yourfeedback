0aec73f67b0edef37adf88e88c5966200b782f79
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-26 15:51:39 +0200

    Frontend <> Editing box component style

--- frontend/frontend-website/yourfeedback-website/src/components/Box.js
+++ frontend/frontend-website/yourfeedback-website/src/components/Box.js
@@ -1,8 +1,10 @@
 const Box = (props) =>{
     return(
-        <div className="flex flex-col items-center justify-around bg-white rounded-xl shadow-2xl w-3/4 h-44 mb-4 sm:w-2/4 lg:w-1/4">
-            <h1 className="text-4xl font-bold">{props.text}</h1>
-            <h1 className="text-4xl font-bold">{props.value}</h1>
+        <div className="flex flex-col items-center justify-between bg-white rounded-xl shadow-2xl w-3/4 h-40 mb-4 sm:w-2/4 lg:w-1/4">
+            <div className="w-full h-16 bg-red-700 rounded-tr-lg rounded-tl-lg flex items-center justify-center">
+                <h1 className="text-2xl font-bold text-white">{props.text}</h1>
+            </div>
+            <h1 className="text-4xl font-bold mb-6">{props.value}</h1>
         </div>
     )
 }
