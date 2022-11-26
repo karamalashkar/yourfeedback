c61beeaff46e06a7e9fc07ba3794d10a5444e18e
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-26 16:33:38 +0200

    Frontend <> Editing edit profile component

--- frontend/frontend-website/yourfeedback-website/src/components/Editprofile.js
+++ frontend/frontend-website/yourfeedback-website/src/components/Editprofile.js
@@ -8,11 +8,12 @@ const Editprofile = ({open,onClose}) =>{
     const business_id=localStorage.getItem('id');
     const[name,setName]=useState('')
     const[bio,setBio]=useState('')
+    const[location,setLocation]=useState('')
     const[image,setImage]=useState('')
     
     const edit = async (e)=>{
         e.preventDefault();
-        const post={business_id,name,bio,image}
+        const post={business_id,name,bio,location,image}
         if(!image){
             post.image=''
         }
@@ -25,6 +26,7 @@ const Editprofile = ({open,onClose}) =>{
             const result=await getBusiness(business_id);
             setName(result.name)
             setBio(result.bio)
+            setLocation(result.location)
         }
 
         business();
@@ -57,8 +59,10 @@ const Editprofile = ({open,onClose}) =>{
 
     return(
         <>
-        <div className="h-auto rounded-xl shadow-2xl bg-white absolute mx-4 w-3/4 md:mx-24 md:w-2/4 xl:mx-52 xl:w-1/2">
-            <button className="mx-8 my-4 text-3xl text-red-700" onClick={onClose}><AiFillCloseCircle/></button>
+        <div className="h-auto rounded-xl shadow-2xl bg-white absolute mx-4 w-3/5 md:mx-24 md:w-2/4 xl:mx-52 xl:w-1/2">
+            <div className="flex justify-end">
+                <button className="right-1 mx-8 my-2 text-3xl text-red-700" onClick={onClose}><AiFillCloseCircle/></button>
+            </div>
             <div className="flex flex-col items-center">
                 <h1 className="text-3xl font-bold mb-4">Edit Profile</h1>
                 <form onSubmit={edit} className="flex flex-col items-center w-full">
@@ -66,16 +70,21 @@ const Editprofile = ({open,onClose}) =>{
                     <input type="text" placeholder="Name" 
                     onChange={(event)=>{setName(event.target.value)}}
                     defaultValue={name}
-                    className="w-1/2 p-3 mt-4 border-2 border-grey-100 rounded-xl"/>
+                    className="w-3/5 p-3 border-2 border-grey-100 rounded-xl"/>
                     
                     <textarea placeholder="Bio" maxLength={200}
                     onChange={(event)=>{setBio(event.target.value)}}
                     defaultValue={bio}
-                    className="w-1/2 p-3 mt-4 mb-4 h-44 border-2 border-grey-100 rounded-xl"></textarea>
+                    className="w-3/5 p-3 mt-2 h-32 border-2 border-grey-100 rounded-xl"></textarea>
                     
+                    <input type="text" placeholder="Location" 
+                    onChange={(event)=>{setLocation(event.target.value)}}
+                    defaultValue={location}
+                    className="w-3/5 p-3 mt-2 border-2 border-grey-100 rounded-xl"/>
+
                     <input type="file" accept="image/png, image/jpg" 
                     onChange={(e)=>{uploadImage(e)}}
-                    className="w-1/2 p-3 border-2 border-grey-100 rounded-xl" />
+                    className="w-3/5 p-3 mt-2 border-2 border-grey-100 rounded-xl" />
 
                     <Button text={'Save'} />
                     <br/>
