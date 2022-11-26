6aad3593b1ba23e5d14116b028dcf1a05ebe7dac
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-26 15:47:43 +0200

    Frontend <> Editing button component style

--- frontend/frontend-website/yourfeedback-website/src/components/Button.js
+++ frontend/frontend-website/yourfeedback-website/src/components/Button.js
@@ -1,6 +1,6 @@
 const Button = (props) =>{
     return(
-        <button className='w-1/2 p-3 mt-4 bg-red-700 text-white rounded-xl'>{props.text}</button>
+        <button className='w-3/5 p-3 mt-4 bg-red-700 text-white font-bold rounded-xl'>{props.text}</button>
     )
 }
 
