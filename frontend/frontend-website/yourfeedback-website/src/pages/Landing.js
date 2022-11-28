94be5071d36027434ab6f36260fd5c570e62e915
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-11-28 19:15:48 +0200

    Frontend <> Editing landing page style

--- frontend/frontend-website/yourfeedback-website/src/pages/Landing.js
+++ frontend/frontend-website/yourfeedback-website/src/pages/Landing.js
@@ -31,19 +31,19 @@ const Landing = () =>{
     
     return(
         <div className="w-full h-full fixed bg-white flex items-center justify-center">
-            <div className='w-4/5 h-4/5 fixed bg-white flex flex-col align-center xl:flex-row drop-shadow-2xl rounded-lg'>
-                <div className="w-2/4 flex flex-col items-center justify-center">
-                    <h1 className="mb-8 font-bold text-3xl">Login</h1>
+            <div className='w-full h-full fixed bg-white flex flex-col-reverse align-center items-center xl:h-4/5 xl:w-4/5 xl:flex-row drop-shadow-2xl rounded-lg'>
+                <div className="w-full flex flex-col items-center justify-center xl:w-2/4">
+                    <h1 className="mb-8 font-bold text-3xl mt-4 xl:mt-0">Login</h1>
                     <h2 className="text-md text-red-700">{error}</h2>
                     <form onSubmit={loginBusiness} className="w-full flex flex-col items-center">
                         <Input type='email' text='Email' setValue={setEmail} setError={setError} />
                         <Input type='password' text='Password' setValue={setPassword} setError={setError} />
                         <Button text='Sign In' />
                     </form>    
-                    <h4 className="my-2 text-sm font-semibold">Don't have an account? <a href='/signup' className="text-red-700">Join Now</a></h4>
+                    <h4 className="my-2 text-sm font-semibold mb-4 xl:mb-0">Don't have an account? <a href='/signup' className="text-red-700">Join Now</a></h4>
                 </div>
-                <div className="w-2/4 h-full bg-red-700 flex flex-col items-center rounded-tr-lg rounded-br-lg">
-                    <img src='darkLogo.png' className='h-1/4 w-2/5 mt-20' alt='feedback' />
+                <div className="w-full h-full bg-red-700 flex flex-col items-center rounded-lg xl:w-2/4 rounded-tr-lg rounded-br-lg">
+                    <img src='darkLogo.png' className='h-1/4 w-2/5 mt-8 xl:mt-20' alt='feedback' />
                     <h1 className="text-white text-opacity-80 w-3/4 mt-8">
                         Your feedback website is for business owner to start receiving feedback from their 
                         customer's according to a feedback code.
